4f2b123ff02f513e4e52425a3b756db878300917
DeliciousM1lk <74536709+DeliciousM1lk@users.noreply.github.com>
2025-01-18 21:45:23 +0500

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def setup_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    return webdriver.Chrome(options=options)
+
+
+def search_jobs():
+    driver = setup_driver()
+
+    try:
+        driver.get("https://hh.kz")
+
+        time.sleep(1)
+
+        search_box = driver.find_element(By.NAME, "text")
+        search_box.send_keys("Web-разработчик")
+        search_box.send_keys(Keys.RETURN)
+
+        time.sleep(2)
+
+        close_button = driver.find_element(By.CLASS_NAME, "bloko-modal-close-button")
+        close_button.click()
+
+        time.sleep(5)
+
+        change_city_button = driver.find_element(By.CLASS_NAME, "supernova-navi-item_area-switcher-button")
+        change_city_button.click()
+
+        time.sleep(1)
+
+        cities = driver.find_elements(By.CLASS_NAME, "bloko-link")
+        almaty_link = None
+        for city in cities:
+            if city.text == "Алматы":
+                almaty_link = city
+        almaty_link.click()
+
+        time.sleep(5)
+
+        jobs = driver.find_elements(By.CLASS_NAME, "magritte-redesign")
+        jobs_data = []
+
+        for job in jobs:
+            try:
+                title = job.find_element(By.CSS_SELECTOR,
+                                         'span[data-qa="serp-item__title-text"]').text
+
+                try:
+                    zp = job.find_element(By.CLASS_NAME, "magritte-text___pbpft_3-0-22").text
+                except Exception:
+                    zp = ''
+
+                company = job.find_element(By.CSS_SELECTOR,
+                                           'span[data-qa="vacancy-serp__vacancy-employer-text"]').text
+                link = job.find_element(By.CLASS_NAME, "magritte-link___b4rEM_4-3-16").get_attribute("href")
+
+                jobs_data.append({
+                    "title": title,
+                    "zp": zp,
+                    "company": company,
+                    "link": link,
+                })
+
+                print(f'Данные о вакансии {title} получены')
+            except  Exception as e:
+                print(f"Ошибка при обработке вакансии: {e}")
+
+        with open("jobs_data.txt", "w", encoding="utf-8") as file:
+            for job in jobs_data:
+                file.write(f"Название: {job['title']}\n")
+                file.write(f"Зарплата: {job['zp']}\n")
+                file.write(f"Компания: {job['company']}\n")
+                file.write(f"Ссылка: {job['link']}\n")
+                file.write("\n")
+        print("Данные успешно сохранены в файл 'jobs_data.txt'")
+
+    finally:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    search_jobs()
